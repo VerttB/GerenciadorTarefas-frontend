0fe7b54602ad69014405e94a60864b6a308de6c2
verttb <alyssonoliveira456@gmail.com>
2024-06-13 16:36:31 -0300

    cores meramente ilustrativas

--- gerenciadorTarefas-frontend/src/components/Header.jsx
+++ gerenciadorTarefas-frontend/src/components/Header.jsx
@@ -6,18 +6,20 @@ function Header(){
     return(
         <>
         <header>
-            <div className="empresa_info">
+            <div className="header_empresa_info">
                 <i>Logo</i>
                 <p>Nome Empresa</p>
+                </div>
 
+            <div className='header_extras'>
                 <ul>
                     <li><button>Sign Up</button></li>
                     <li><button>Sing Out</button></li>
+                    <li><a href="">Sobre</a></li>
                 </ul>
-
                 <input type="checkbox" id="slideTema" />
-                <label htmlFor="slideTema"></label>
-            </div>
+                <label htmlFor="slideTema" id="temaLabel"></label>
+                </div>
         </header>
     </>
     )
