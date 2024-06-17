245f91bc8523bc9ae305fd4bd4486f797293fbe4
verttb <alyssonoliveira456@gmail.com>
2024-06-17 12:18:50 -0300

    teste cadastro

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -12,12 +12,15 @@ function App(){
           <FacebookFilled className='facebookIcon' />
           <LinkedinFilled className='linkedinIcon'/>
       </div>
+      <Space direction='vertical'>
+       <Input placeholder="Nome" prefix={<UserOutlined  />} />
         <Space direction='vertical'>
        <Input placeholder="Email" prefix={<MailOutlined  />} />
        <Space>
       <Input.Password placeholder="Senha" prefix={<LockOutlined/>}/>
       </Space>
       </Space>  
+      </Space>
 
       <Button>Sign Up</Button>
   
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Login from './components/Login.jsx'
+import Cadastro from './components/Cadastro.jsx'
 import './assets/styles/reset.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
     {/* <Header/> */}
-    <Login></Login>
+    {/* <Login></Login> */}
+    <Cadastro></Cadastro>
     {/* <Footer></Footer> */}
     </>
   </React.StrictMode>,
