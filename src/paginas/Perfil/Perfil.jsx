7c543fd2e2c4ba3840f6bb0b6d54d04100ddb58c
verttb <alyssonoliveira456@gmail.com>
2024-07-08 04:23:48 -0300

    userid

--- src/paginas/Perfil/Perfil.jsx
+++ src/paginas/Perfil/Perfil.jsx
@@ -11,7 +11,7 @@ function Perfil({ user, setUser }) {
     const [email, setEmail] = useState(user.email);
 
     const modificarPerfil = () => {
-        const userNovo = { nome: nome, senha: senha, email: email };
+        const userNovo = {userId: user.userId ,nome: nome, senha: senha, email: email };
         fetch(`http://localhost:8080/GerenciadorTarefas/${user.userId}/editar_conta`, {
             method: 'PUT',
             headers: {
